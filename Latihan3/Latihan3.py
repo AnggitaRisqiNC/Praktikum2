edd94a0875769319bd7c5d8a1fb7d8b97ea368ea
AnggitaRisqiNC <anggitarisqi0907@gmail.com>
2022-10-19 19:38:53 +0700

    Menambahkan Latihan3.py

---
+++ Latihan3/Latihan3.py
@@ -0,0 +1,16 @@
+# input nilai variabel
+a=input("Masukkan nilai a: ")
+b=input("Masukkan nilai b: ")
+
+# cetak nilai variabel
+print("Variabel a= ",a)
+print("Variabel b= ",b)
+
+# cetak hasil operasi kedua variabel dengan String Format
+str(print("Hasil Penggabungan 6&8= ",a+b))
+
+# konversi nilai variabel
+a=int(a)
+b=int(b)
+str(print("Hasil Penggabungan 6+8= ",a+b))
+str(print("Hasil Penggabungan 6/8= ",a/b))
